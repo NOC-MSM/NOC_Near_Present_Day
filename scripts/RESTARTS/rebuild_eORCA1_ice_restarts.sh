34d552476bf292f5758243df43ce7985db3c1a42
Ollie Tooth <oliver.tooth@noc.ac.uk>
2025-10-23 15:42:26 +0100

    Update scripts to rebuild eORCA1 & eORCA025 ocean-ice restart files.

--- scripts/RESTARTS/rebuild_eORCA1_ice_restarts.sh
+++ scripts/RESTARTS/rebuild_eORCA1_ice_restarts.sh
@@ -15,37 +15,39 @@ module purge
 module load NEMO/prg-env
 
 # -- Inputs -- #
+# Define path to NEMO directory in NOC_Near_Present_Day:
+nemo_dir=..../NOC_Near_Present_Day/nemo
 # Define restart directory and collect unique restart files:
-restart_dir=..../NOC_Near_Present_Day/nemo/cfgs/GLOBAL_QCO/eORCA025/RESTARTS
+restart_dir=$nemo_dir/cfgs/GLOBAL_QCO/eORCA1/RESTARTS/
 # Define output directory for rebuilt restart files:
-output_dir=/dssgfs01/scratch/npd/restarts/eORCA025_ERA5_v1
+output_dir=/dssgfs01/scratch/npd/restarts/eORCA1_ERA5_v1/
 
 # Define number of domains:
-ndom=1326
+ndom=552
 # Define start year:
 yr=1976
 # Define initial counter value:
 n=0
 
-# Extract restart file list:
+# Get restart file list:
 cd $restart_dir
-restart_files=$(find eORCA025_ERA5_*_restart_ice_0000.nc)
+restart_files=$(find eORCA1_ERA5_*_restart_ice_0000.nc)
 
 # Iterate over restart files and rebuild:
 for restart_file in $restart_files; do
-    # Extract prefix for ice restart file:
-    restart_prefix=${restart_file::34}
+    # Get prefix for restart file:
+    restart_prefix=${restart_file::28}
     
     # Rebuild ice restart file:
     echo "In Progress: Rebuilding -> $restart_prefix"
-    ..../NOC_Near_Present_Day/nemo/tools/REBUILD_NEMO/rebuild_nemo -m $restart_prefix $ndom
+    $nemo_dir/tools/REBUILD_NEMO/rebuild_nemo -m $restart_prefix $ndom
     
     # Move rebuilt ice restart file to NPD restarts directory:
     if [[ $n -lt 2 ]]; then
         # Spin-Up restart files:
-        mv $restart_prefix.nc $output_dir/${restart_prefix::8}_${yr}12_${restart_prefix:14}.nc
+        mv $restart_prefix.nc $output_dir/${restart_prefix::11}_${yr}12_${restart_prefix:14}.nc
     else
-        mv $restart_prefix.nc $output_dir/${restart_prefix::8}_${yr}12_${restart_prefix:14}.nc
+        mv $restart_prefix.nc $output_dir/${restart_prefix::11}_${yr}12_${restart_prefix:14}.nc
         # Increment year:
         yr=$((yr+1))
     fi
@@ -53,4 +55,3 @@ for restart_file in $restart_files; do
     # Increment counter:
     n=$((n+1))
 done
-
--- scripts/RESTARTS/rebuild_eORCA1_ocean_restarts.sh
+++ scripts/RESTARTS/rebuild_eORCA1_ocean_restarts.sh
@@ -4,7 +4,7 @@
 # rebuild_eORCA1_ocean_restarts.slurm
 #
 # Description: Script to rebuild NPD-eORCA1-ERA5v1 ocean restart
-# files from subdomains in the RESTART/ directory.
+# files from tiles in the RESTART/ directory.
 #
 # Created By: Ollie Tooth (oliver.tooth@noc.ac.uk)
 # Created On: 2025-04-03
@@ -15,37 +15,39 @@ module purge
 module load NEMO/prg-env
 
 # -- Inputs -- #
+# Define path to NEMO directory in NOC_Near_Present_Day:
+nemo_dir=..../NOC_Near_Present_Day/nemo
 # Define restart directory and collect unique restart files:
-restart_dir=..../NOC_Near_Present_Day/nemo/cfgs/GLOBAL_QCO/eORCA025/RESTARTS
+restart_dir=$nemo_dir/cfgs/GLOBAL_QCO/eORCA1/RESTARTS/
 # Define output directory for rebuilt restart files:
-output_dir=/dssgfs01/scratch/npd/restarts/eORCA025_ERA5_v1
+output_dir=/dssgfs01/scratch/npd/restarts/eORCA1_ERA5_v1/
 
 # Define number of domains:
-ndom=1326
+ndom=552
 # Define start year:
 yr=1976
 # Define initial counter value:
 n=0
 
-# Extract restart file list:
+# Get restart file list:
 cd $restart_dir
-restart_files=$(find eORCA025_ERA5_*_restart_0000.nc)
+restart_files=$(find eORCA1_ERA5_*_restart_0000.nc)
 
 # Iterate over restart files and rebuild:
 for restart_file in $restart_files; do
-    # Extract prefix for ice restart file:
-    restart_prefix=${restart_file::30}
+    # Get prefix for restart file:
+    restart_prefix=${restart_file::28}
     
     # Rebuild ocean restart file:
     echo "In Progress: Rebuilding -> $restart_prefix"
-    ..../NOC_Near_Present_Day/nemo/tools/REBUILD_NEMO/rebuild_nemo -m $restart_prefix $ndom
+    $nemo_dir/tools/REBUILD_NEMO/rebuild_nemo -m $restart_prefix $ndom
     
     # Move rebuilt ocean restart file to NPD restarts directory:
     if [[ $n -lt 2 ]]; then
         # Spin-up restart files:
-        mv $restart_prefix.nc $output_dir/${restart_prefix::8}_${yr}12_${restart_prefix:14}.nc
+        mv $restart_prefix.nc $output_dir/${restart_prefix::11}_${yr}12_${restart_prefix:14}.nc
     else
-        mv $restart_prefix.nc $output_dir/${restart_prefix::8}_${yr}12_${restart_prefix:14}.nc
+        mv $restart_prefix.nc $output_dir/${restart_prefix::11}_${yr}12_${restart_prefix:14}.nc
         # Increment year:
         yr=$((yr+1))
     fi
@@ -53,4 +55,3 @@ for restart_file in $restart_files; do
     # Increment counter:
     n=$((n+1))
 done
-
